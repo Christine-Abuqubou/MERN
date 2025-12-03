33c6b416c16212f2f54f6bb97141f533d8705f57
chris bsharah <chrisbsharah@chriss-MacBook-Air.local>
2025-12-03 11:23:35 +0200

    sensi inherit

--- ninja.js
+++ ninja.js
@@ -23,9 +23,24 @@ class Ninja{
 
 
 }
+class Sensei extends Ninja{
+    constructor(name){
+        super(name,200,10,10);
+        this.wisdom=10;
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Wisdom is the key to success.");
+
+    }
+}
 console.log("Ninja class loaded successfully.");
 let ninja1 = new Ninja("Naruto", 100);
 ninja1.sayName();       
 ninja1.showStats();    
 ninja1.drinkSake();     
 ninja1.showStats();     
+let sensi1= new Sensei("coockie");
+sensi1.speakWisdom();
+sensi1.showStats();
+console.log("Sensei class loaded successfully.");
